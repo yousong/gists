2e09b2cd05c44d9201e2301fca910928ce8e5153
Yousong Zhou <yszhou4tech@gmail.com>
2017-10-26 15:03:30 +0800

    python/apnic-parse.py: convert to network prefix

--- python/apnic-parse.py
+++ python/apnic-parse.py
@@ -1,3 +1,6 @@
+import iputils
+import sys
+
 def parseLine(line):
     fields = line.split('|')
     if len(fields) != 7:
@@ -28,55 +31,49 @@ def filterFuncIPv4(ent):
 def filterFuncIPv4CN(ent):
     return ent['type'] == 'ipv4' and ent['cc'] == 'CN'
 
-def ipToNum(ip):
-    ds = [int(i) for i in ip.split('.')]
-    num = ds[0] << 24
-    num += ds[1] << 16
-    num += ds[2] << 8
-    num += ds[3]
-    return num
-
-def num2IP(num):
-    num = num & 0xffffffff
-    return '%d.%d.%d.%d' % (num >> 24, (num >> 16) & 0xff, (num >> 8) & 0xff, num & 0xff)
-
-
-def mergeIPv4(ents, filterFunc=None):
+def IpPairs(ents, filterFunc=None, sort=False):
     if filterFunc is None:
         filterFunc = filterFuncIPv4
-    ipPairs = []
+    pairs = []
     for ent in ents:
         if not filterFunc(ent):
             continue
-        ipNum = ipToNum(ent['start'])
+        ipNum = iputils.ip2num(ent['start'])
         value = int(ent['value'])
-        ipPairs.append((ipNum, ipNum + value))
+        pairs.append((ipNum, ipNum + value))
 
-    ipPairsSorted = sorted(ipPairs, cmp=lambda a, b: a[0] < b[0])
-    if len(ipPairsSorted) <= 1:
-        return ipPairsSorted
-    ipPairsDense0 = []
-    lastStart, lastEnd = ipPairsSorted[0]
-    for start, end in ipPairsSorted[1:]:
+    if sort:
+        pairs = sorted(pairs, cmp=lambda a, b: a[0] < b[0])
+    return pairs
+
+def IpPairsMerge(pairs):
+    if len(pairs) <= 1:
+        return pairs
+    pairsDensed = []
+    lastStart, lastEnd = pairs[0]
+    for start, end in pairs[1:]:
         if lastEnd == start:
             lastEnd = end
         else:
-            ipPairsDense0.append((lastStart, lastEnd))
+            pairsDensed.append((lastStart, lastEnd))
             lastStart = start
             lastEnd = end
-    ipPairsDense0.append((lastStart, lastEnd))
+    pairsDensed.append((lastStart, lastEnd))
+    return pairsDensed
 
-    lastStart, lastEnd = ipPairsDense0[0]
-    for start, end in ipPairsDense0[1:]:
-        print num2IP(lastStart), num2IP(lastEnd), lastEnd - lastStart, start - lastEnd
-        lastStart, lastEnd = start, end
-    print num2IP(lastStart), num2IP(lastEnd), lastEnd - lastStart
-    print len(ipPairs), len(ipPairsSorted), len(ipPairsDense0)
-    # As of 2017-10-20
-    #
-    #   8017 8017 3559
+def IpPairsToNetPreflen(pairs):
+    netprefs = []
+    for start, end in pairs:
+        for n, preflen in iputils.range2preflen(start, end - 1):
+            net = iputils.num2ip(n)
+            netprefs.append((net, preflen))
+    return netprefs
 
-# https://ftp.apnic.net/stats/apnic/delegated-apnic-latest
+# wget -c https://ftp.apnic.net/stats/apnic/delegated-apnic-latest
 filename = "delegated-apnic-latest"
 ents = parseFile(filename)
-mergeIPv4(ents, filterFunc=filterFuncIPv4CN)
+pairs = IpPairs(ents, filterFunc=filterFuncIPv4CN, sort=True)
+pairs = IpPairsMerge(pairs)
+netprefs = IpPairsToNetPreflen(pairs)
+for net, preflen in netprefs:
+    sys.stdout.write("%s/%d\n" % (net, preflen))
