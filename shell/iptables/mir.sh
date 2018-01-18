97d69a96e65bcd9527e01559c655d1066d394946
Yousong Zhou <yszhou4tech@gmail.com>
2018-01-18 17:07:47 +0800

    iptables/mir.sh: update
     - net.ipv4.ip_forward=1
     - make iptables action more clear

--- shell/iptables/mir.sh
+++ shell/iptables/mir.sh
@@ -9,21 +9,21 @@ for n in $mir_nets; do
 	ipset add -! mirnet "$n"
 done
 
-while iptables -t nat -D PREROUTING  -j mirpre  2>/dev/null; do :; done
-while iptables -t nat -D POSTROUTING -j mirpost 2>/dev/null; do :; done
-iptables -t nat -F mirpre
-iptables -t nat -F mirpost
-iptables -t nat -X mirpre
-iptables -t nat -X mirpost
+sysctl -w net.ipv4.ip_forward=1
 
-iptables -t nat -N mirpre
-iptables -t nat -A mirpre -m set ! --match-set mirnet src -j RETURN
-iptables -t nat -A mirpre -p udp -m multiport --dports "$mir_ports" -j DNAT --to-destination "$mir_ip"
-iptables -t nat -A mirpre -p tcp -m multiport --dports "$mir_ports" -j DNAT --to-destination "$mir_ip"
-iptables -t nat -A PREROUTING -j mirpre
+iptables-save --counters \
+	| grep -vE '(mirpre|mirpost)' \
+	| iptables-restore --counters
 
-iptables -t nat -N mirpost
-iptables -t nat -A mirpre -m set ! --match-set mirnet src -j RETURN
-iptables -t nat -A mirpost -p udp -m multiport --dports "$mir_ports" -j MASQUERADE
-iptables -t nat -A mirpost -p tcp -m multiport --dports "$mir_ports" -j MASQUERADE
-iptables -t nat -A POSTROUTING -j mirpost
+iptables-restore --noflush <<-"EOF"
+	*nat
+	:mirpre -
+	:mirpost -
+	-A mirpre -p udp -m multiport --dports 65534,65535,21841 -j DNAT --to-destination 104.238.138.31
+	-A mirpre -p tcp -m multiport --dports 65534,65535,21841 -j DNAT --to-destination 104.238.138.31
+	-A mirpost -p udp -m multiport --dports 65534,65535,21841 -j MASQUERADE
+	-A mirpost -p tcp -m multiport --dports 65534,65535,21841 -j MASQUERADE
+	-A PREROUTING -m set --match-set mirnet src -m addrtype ! --src-type LOCAL -j mirpre
+	-A POSTROUTING -m set --match-set mirnet src -m addrtype ! --src-type LOCAL -j mirpost
+	COMMIT
+EOF
