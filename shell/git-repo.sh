38e713baa6c89e78573f80bdc6ca7bb1cad7ed5a
Yousong Zhou <yszhou4tech@gmail.com>
2018-07-03 21:33:19 +0800

    git-repo.sh: o_ignor prefix match support

--- shell/git-repo.sh
+++ shell/git-repo.sh
@@ -1,5 +1,17 @@
 o_grepo=$HOME/git-repo
-o_repos=$(find $o_grepo -maxdepth 3 -name .git -type d)
+o_ignor="lede-project/ ghtar/ gcc/"
+o_repos=
+for r0 in $(find $o_grepo -maxdepth 3 -name .git -type d); do
+	r="${r0#$o_grepo/}"
+	no=0
+	for r1 in $o_ignor; do
+		if [ "$r" != "${r#$r1}" ]; then
+			no=1
+			break
+		fi
+	done
+	[ "$no" -eq 0 ] && o_repos="$o_repos $r0"
+done
 o_repod=$(for d in $o_repos; do echo ${d%/.git}; done)
 o_packd=$HOME/packed
 
@@ -32,11 +44,11 @@ gc() {
 }
 
 pk() {
-	mkdir -p $o_packed
+	mkdir -p $o_packd
 	for d in $o_repod; do
 		echo "### $d"
 		n=$(basename $d).$(cd $d; git rev-list --oneline --pretty='%cd.%h' --date=short  -1 HEAD | tail -n1).tar.xz
-		f="$o_packed/$n"
+		f="$o_packd/$n"
 		[ -s "$f" ] && continue
 		rm -vf "$f.t"
 		XZ_OPT=-T0 tar -cJf "$f.t" "${d#$PWD/}/.git"
@@ -45,9 +57,9 @@ pk() {
 }
 
 rs() {
-	[ -d "$o_packed" ] || return 1
+	[ -d "$o_packd" ] || return 1
 	mkdir -p "$o_grepo"
-	for f in "$o_packed"/*; do
+	for f in "$o_packd"/*; do
 		echo "### $f"
 		tar -C "$o_grepo" -xJf "$f"
 	done
