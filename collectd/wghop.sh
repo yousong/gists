947b2e10208e9d7fc802a433791c423c64819271
Yousong Zhou <yszhou4tech@gmail.com>
2019-11-13 11:44:30 +0800

    collectd: rand src and rand dst

--- collectd/wghop.sh
+++ collectd/wghop.sh
@@ -10,11 +10,53 @@ prep() {
 }
 
 rand() {
-	hexdump -n 2 -e '1/2 "%u\n"' /dev/urandom
+	local n=$1
+	local expr="1/$n"' "%u\n"'
+	hexdump -n "$n" -e "$expr" /dev/urandom
 }
 
+rand_range() {
+	local n="$1"; shift
+	local s="$1"; shift
+	local e="$1"; shift
+	local r
+
+	r="$(rand "$n")"
+	echo "$(($s + $r % ($e - $s + 1)))"
+}
+
+peer=""
+endpoint_host0=
+endpoint_host1=
+endpoint_host=$endpoint_host0
+endpoint_port=21841
+
+rand_src_port=1
+rand_dst_port=1
+
 hop() {
-	while ! wg set wg0 listen-port $((20000+($(rand)%(65536-20000)))); do :; done
+	local srcport dstport
+	local args
+
+	if [ -z "$endpoint" -o -z "$peer" ]; then
+		return
+	fi
+	if [ -n "$rand_src_port" ]; then
+		srcport="$(rand_range 2 20000 65536)"
+		args="$args listen-port $srcport"
+	fi
+	if [ -n "$rand_dst_port" ]; then
+		dstport="$(rand_range 2 21841 41841)"
+	else
+		dstport="$endpoint_port"
+	fi
+	endpoint="$endpoint_host:$dstport"
+	args="$args peer $peer endpoint $endpoint"
+
+	set -x
+	wg set wg0 $args
+	: /etc/init.d/collectd restart
+	set +x
 }
 
 serv() {
