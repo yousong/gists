22542af0396cca635d16b6566e76b242b6a38078
Yousong Zhou <yszhou4tech@gmail.com>
2020-08-05 10:55:59 +0800

    distro-on-qemu: add prep_default_centos_yumrepo

--- qemu/distro-on-qemu/a.sh
+++ qemu/distro-on-qemu/a.sh
@@ -178,6 +178,75 @@ prep_default_ubuntu_sourceslist() {
 	chown 0:0 "$rootdir/etc/apt/sources.list"
 }
 
+prep_default_centos_yumrepo() {
+	[ -n "$distro_version_id" ]
+	sed -e "s/distro_version_id/$distro_version_id/" <<-"EOF" >"$rootdir/etc/yum.repos.d/CentOS-Base.repo"
+	# CentOS-Base.repo
+	#
+	# The mirror system uses the connecting IP address of the client and the
+	# update status of each mirror to pick mirrors that are updated to and
+	# geographically close to the client.  You should use this for CentOS updates
+	# unless you are manually picking other mirrors.
+	#
+	# If the mirrorlist= does not work for you, as a fall back you can try the 
+	# remarked out baseurl= line instead.
+	#
+	#
+	 
+	[base]
+	name=CentOS-$releasever - Base - mirrors.aliyun.com
+	failovermethod=priority
+	baseurl=http://mirrors.aliyun.com/centos/$releasever/os/$basearch/
+		http://mirrors.aliyuncs.com/centos/$releasever/os/$basearch/
+		http://mirrors.cloud.aliyuncs.com/centos/$releasever/os/$basearch/
+	gpgcheck=1
+	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-distro_version_id
+	 
+	#released updates 
+	[updates]
+	name=CentOS-$releasever - Updates - mirrors.aliyun.com
+	failovermethod=priority
+	baseurl=http://mirrors.aliyun.com/centos/$releasever/updates/$basearch/
+		http://mirrors.aliyuncs.com/centos/$releasever/updates/$basearch/
+		http://mirrors.cloud.aliyuncs.com/centos/$releasever/updates/$basearch/
+	gpgcheck=1
+	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-distro_version_id
+	 
+	#additional packages that may be useful
+	[extras]
+	name=CentOS-$releasever - Extras - mirrors.aliyun.com
+	failovermethod=priority
+	baseurl=http://mirrors.aliyun.com/centos/$releasever/extras/$basearch/
+		http://mirrors.aliyuncs.com/centos/$releasever/extras/$basearch/
+		http://mirrors.cloud.aliyuncs.com/centos/$releasever/extras/$basearch/
+	gpgcheck=1
+	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-distro_version_id
+	 
+	#additional packages that extend functionality of existing packages
+	[centosplus]
+	name=CentOS-$releasever - Plus - mirrors.aliyun.com
+	failovermethod=priority
+	baseurl=http://mirrors.aliyun.com/centos/$releasever/centosplus/$basearch/
+		http://mirrors.aliyuncs.com/centos/$releasever/centosplus/$basearch/
+		http://mirrors.cloud.aliyuncs.com/centos/$releasever/centosplus/$basearch/
+	gpgcheck=1
+	enabled=0
+	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-distro_version_id
+	 
+	#contrib - packages by Centos Users
+	[contrib]
+	name=CentOS-$releasever - Contrib - mirrors.aliyun.com
+	failovermethod=priority
+	baseurl=http://mirrors.aliyun.com/centos/$releasever/contrib/$basearch/
+		http://mirrors.aliyuncs.com/centos/$releasever/contrib/$basearch/
+		http://mirrors.cloud.aliyuncs.com/centos/$releasever/contrib/$basearch/
+	gpgcheck=1
+	enabled=0
+	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-distro_version_id
+	EOF
+	chown 0:0 "$rootdir/etc/yum.repos.d/CentOS-Base.repo"
+}
+
 detect_rootfs() {
 	local osrelf="$rootdir/etc/os-release"
 	local NAME VERSION ID VERSION_ID VERSION_CODENAME
@@ -324,8 +393,7 @@ preproot() {
 			fi
 			;;
 		centos)
-			cat "$topdir/Centos-7.repo" >"$rootdir/etc/yum.repos.d/CentOS-Base.repo"
-			chown 0:0 "$rootdir/etc/yum.repos.d/CentOS-Base.repo"
+			prep_default_centos_yumrepo
 			touch "$rootdir/.autorelabel"
 			;;
 		fedora)
