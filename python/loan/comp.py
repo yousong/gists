3019379137afb1ff1e70f0ae2508ec99212b882d
Yousong Zhou <yszhou4tech@gmail.com>
2023-04-17 17:55:42 +0800

    loan: add comp_eq_each_month

--- python/loan/comp.py
+++ python/loan/comp.py
@@ -18,12 +18,14 @@ def comp_x(debt, n_month, month_rate):
 def comp_eq_pr(debt, n_month, month_rate):
     each_month = comp_x(debt, n_month, month_rate)
     sum_interest = 0
-    while debt > 0:
+    i = 1
+    while debt > 0.01:
         interest = debt * month_rate
         each_p = each_month - interest
-        print '%9.2f %9.2f %9.2f' % (each_month, sum_interest, interest)
         sum_interest += interest
         debt -= each_p
+        print(f'{i:3d} {each_month:9.2f} {each_p:9.2f} {interest:9.2f} {sum_interest:9.2f} {debt:9.2f}')
+        i += 1
     return sum_interest
 
 # 还款序列：等值本金
@@ -36,20 +38,43 @@ def comp_eq_pr(debt, n_month, month_rate):
 def comp_eq_p(debt, n_month, month_rate):
     each_p = debt / n_month
     sum_interest = 0
-    while debt > 0:
+    i = 1
+    while debt > 0.01:
         interest = debt * month_rate
         each_month = each_p + interest
-        print '%9.2f %9.2f %9.2f' % (each_month, sum_interest, interest)
         sum_interest += interest
         debt -= each_p
+        print(f'{i:3d} {each_month:9.2f} {each_p:9.2f} {interest:9.2f} {sum_interest:9.2f} {debt:9.2f}')
+        i += 1
+    return sum_interest
+
+# 自由还款：约定每月还款额，中途可调整
+def comp_eq_each_month(debt, each_month, month_rate):
+    sum_interest = 0
+    i = 1
+    while debt > 0.01:
+        interest = debt * month_rate
+        each_p = each_month - interest
+        debt -= each_p
+        if debt < 0:
+            debt = 0
+        sum_interest += interest
+        print(f'{i:3d} {each_month:9.2f} {each_p:9.2f} {interest:9.2f} {sum_interest:9.2f} {debt:9.2f}')
+        i += 1
     return sum_interest
 
 # 公基金贷款利率：5年为分界
-def comp_month_rate_cpf(n_month):
-    if n_month <= 60:
-        return 2.75 / 100 / 12
+def comp_month_rate_cpf(n_month, one_more=False):
+    if one_more:
+        if n_month <= 60:
+            return 3.025 / 100 / 12
+        else:
+            return 3.575 / 100 / 12
     else:
-        return 3.25 / 100 / 12
+        if n_month <= 60:
+            return 2.75 / 100 / 12
+        else:
+            return 3.25 / 100 / 12
 
 debt = 300000
 
