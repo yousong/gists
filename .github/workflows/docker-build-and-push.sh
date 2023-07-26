1fbec9f01df51c5d190578922421dfb59d7018e1
Yousong Zhou <yszhou4tech@gmail.com>
2023-07-26 14:20:23 +0800

    docker: convert to using DOCKER_IMAGE_TAGS.

--- .github/workflows/docker-build-and-push.sh
+++ .github/workflows/docker-build-and-push.sh
@@ -14,22 +14,37 @@ build_and_push() {
 		log "build_and_push: $d: skipping (missing env file)"
 		return
 	fi
-	unset DOCKER_IMAGE_TAG
+
+	unset DOCKER_IMAGE_TAGS
 	source "$d/docker-build-env"
-	if test -z "$DOCKER_IMAGE_TAG"; then
-		log "build_and_push: $d: skipping (empty or null \$DOCKER_IMAGE_TAG)"
-		return
+
+	local tag
+	local built_tag
+	for tag in "${DOCKER_IMAGE_TAGS[@]}"; do
+		if test "$GITHUB_REF" = "refs/heads/testing"; then
+			log "build_and_push: $d: build testing image"
+			tag="${tag%:*}:testing-${tag#*:}"
+		fi
+		if test -z "$built_tag"; then
+			log "build_and_push: $d: build and push $tag"
+			docker build -t "$tag" .
+			docker image push "$tag"
+			built_tag="$tag"
+		else
+			log "build_and_push: $d: tag and push $tag"
+			docker tag "$built_tag" "$tag"
+			docker image push "$tag"
+		fi
+	done
+
+	if test -z "$built_tag"; then
+		log "build_and_push: $d: nothing built (empty or null \$DOCKER_IMAGE_TAGS)"
 	fi
-	if test "$GITHUB_REF" = "refs/heads/testing"; then
-		log "build_and_push: $d: build testing image"
-		DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG%:*}:testing-${DOCKER_IMAGE_TAG#*:}"
-	fi
-	log "build_and_push: $d: building $DOCKER_IMAGE_TAG"
-	docker build -t "$DOCKER_IMAGE_TAG" .
-	docker image push "$DOCKER_IMAGE_TAG"
 }
 
 build_and_push_all() {
+	local d
+
 	cd "$topdir"
 	for d in docker/*; do
 		build_and_push "$topdir/$d"
