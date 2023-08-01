b8248cedb54bade5aa5d5db7b613397d000bdb80
Yousong Zhou <yszhou4tech@gmail.com>
2023-08-01 19:43:02 +0800

    docker: gocryptfs: /dev/fuse device and caps

--- docker/gocryptfs/a.sh
+++ docker/gocryptfs/a.sh
@@ -6,4 +6,4 @@ mkdir -p c p
 if ! test -s "p/.gocryptfs.reverse.conf"; then
 	docker run --rm -it -v /dev/fuse:/dev/fuse --privileged -v $PWD/c:/c -v $PWD/p:/p $i -init -reverse /p
 fi
-docker run --rm -it -v /dev/fuse:/dev/fuse --privileged -v $PWD/c:/c:shared -v $PWD/p:/p:ro -v $PWD/passfile:/passfile:ro $i -fg -allow_other -nosyslog -reverse -passfile /passfile /p /c
+docker run --rm -it --device /dev/fuse --cap-add sys_admin -v $PWD/c:/c:shared -v $PWD/p:/p:ro -v $PWD/passfile:/passfile:ro $i -fg -allow_other -nosyslog -reverse -passfile /passfile /p /c
